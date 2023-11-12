cf07b80d51db518bb843feb6afa8d6899108b140
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2023-11-12 06:27:50 +0530

    #include<stdio.h> int main() {     int i,r,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(r=1;r<=n;r++)         {             printf("%d ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_1.3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,r,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(r=1;r<=n;r++)
+        {
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
