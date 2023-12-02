ee399e8a0006393551221ad9cbaf4bc36036d708
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2023-12-02 09:36:17 +0530

    Add files via upload

---
+++ asci_value.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    printf("%d",ch);
+}
+
+
+//    A(65) ---> a(97)
+//  #include<stdio.h>
+//  int main()
+//  {
+//     char ch;
+//     scanf("%c",&ch);
+//     printf("%c",ch+32);  uppercase --> lowercase (+32)
+//  }                       lowercase --> uppercase (-32)
+
+
+//  %d ---> prints value
+//  %c ---> prints character
+
