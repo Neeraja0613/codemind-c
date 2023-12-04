cd559dba1f7a21d99baf4545c5b20e3e0ec4c5af
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2023-12-04 10:04:49 +0530

    Add files via upload

---
+++ megaprime.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,t,c=0,r,flag=0;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		c=c+1;
+	}
+	if(c==2)
+	t=i;
+	while(t!=0)
+	{
+		r=t%10;
+		if(r==2 || r==3 || r==5 || r==7)
+		{
+			flag=1;
+		}
+		t=t/10;
+	}
+	if(flag==1)
+	{
+	    printf("mega prime");
+    }
+	else
+	{
+	printf("not mega prime");
+    }
+}
