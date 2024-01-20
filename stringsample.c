276535c4740f4f1fa45e1ced640d29bc5933eda2
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-01-20 06:47:32 +0530

    Add files via upload

---
+++ stringsample.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char str[6]={'a','d','i','t','y','a'};
+	char str1[4]="acet";
+	char str2[]="aditya college";
+	printf("%s\n",str);
+	printf("%s\n",str1);
+	printf("%s",str2);
+}
