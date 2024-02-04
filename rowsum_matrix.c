8bff28b7fd060804c0f67e814f18aae550aaa14a
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-02-05 00:02:42 +0530

    Add files via upload

---
+++ rowsum_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int r,c,i,j,rs=0;
+	scanf("%d%d",&r,&c);
+	int a[r][c];
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<r;i++)
+	{
+		rs=0;
+		for(j=0;j<c;j++)
+		{
+			rs=rs+a[i][j];
+		}
+		printf("%d\n",rs);
+	}
+}
