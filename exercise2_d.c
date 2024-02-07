f7e31c41f7eb7203e9c70acc1b04f99f180ad73f
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-02-07 20:47:35 +0530

    Add files via upload

---
+++ exercise2_d.c
@@ -0,0 +1,19 @@
+//2. d;
+/*#include<stdio.h>
+int main()
+{
+float C,F;
+scanf("%f",&C);
+F=(C*9/5)+32;
+printf("%.2f",F);
+}*/
+
+
+#include<stdio.h>
+int main()
+{
+float C,F;
+scanf("%f",&F);
+C=(F-32)*5/9;
+printf("%.2f",C);
+}
