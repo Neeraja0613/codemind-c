28194220e664cef1fa7407aad77c5912077cb2f5
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-02-14 09:43:32 +0530

    Add files via upload

---
+++ maxndminofarray_internal.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,max,min;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	max=arr[0];
+	min=arr[0];
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]>max)
+		{
+			max=arr[i];
+		}
+		if(arr[i]<min)
+		{
+			min=arr[i];
+		}
+	}
+	printf("maximum element: %d\n",max);
+	printf("minimum element: %d",min);
+}
