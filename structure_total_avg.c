ee362648f1d12dbfb267028bc00d63a52884e65b
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-03-09 17:57:31 +0530

    Add files via upload

---
+++ structure_total_avg.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+// Define the structure for student information
+struct Student {
+ char name[50];
+ int marks;
+};
+int main() {
+ int n;
+ // Get the number of students
+ printf("Enter the number of students: ");
+ scanf("%d", &n);
+ // Declare an array of structures to store student information
+ struct Student students[n];
+ // Input information for each student
+ int i;
+ for (i = 0; i < n; i++) {
+ printf("Enter the name of student %d: ", i + 1);
+ scanf("%s", students[i].name);
+ printf("Enter the marks for %s: ", students[i].name);
+ scanf("%d", &students[i].marks);
+ }
+ // Calculate total and average marks
+ int total = 0;
+ int x;
+ for (x = 0; x < n; x++) {
+ total += students[x].marks;
+ }
+ float average = (float)total / n;
+ // Display the total and average marks
+ printf("\nTotal marks: %d\n", total);
+ printf("Average marks: %.2f\n", average);
+ return 0;
+}
+
