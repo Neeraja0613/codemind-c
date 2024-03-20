15ee7b0c87a5a7a99e9f6640e1631df169b636ee
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-03-20 15:45:02 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int s=0,avg;     for(i=0;i<n;i++)     {         s=s+a[i];     }     avg=s/n;     int c=0;     for(i=0;i<n;i++)     {         if(a[i]>=avg)         {             c++;         }     }     printf("%d",c); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int s=0,avg;
+    for(i=0;i<n;i++)
+    {
+        s=s+a[i];
+    }
+    avg=s/n;
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
