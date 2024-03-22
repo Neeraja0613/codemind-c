3a00dea27990869e4f74119333adbd08f583a128
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-03-22 07:31:17 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int c;     for(i=n-1;i>=0;i--)     {         if(a[i]%2==1)         {             c=a[i];             break;         }     }     printf("%d",c); }

---
+++ Last_odd_number_in_an_Array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c;
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]%2==1)
+        {
+            c=a[i];
+            break;
+        }
+    }
+    printf("%d",c);
+}
