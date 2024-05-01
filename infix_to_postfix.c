13c6baf91ceca51263260bfa29791a0312299147
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-05-01 13:00:21 +0530

    Add files via upload

---
+++ infix_to_postfix.c
@@ -0,0 +1,50 @@
+//INFIX ---> POSTFIX
+#include<stdio.h>
+#include<string.h>
+int get_priority(char ch)
+{
+	if(ch=='/' || ch=='*')  return 3;
+	else if(ch=='+' || ch=='-')  return 2;
+	else  return 1;
+}
+void convert_infix_to_postfix(char *infix)
+{
+	char postfix[101];
+	char st[101];
+	int top=-1,i,j=0;
+	for(i=0;i<strlen(infix);i++)
+	{
+		int priority=get_priority(infix[i]);
+		if(priority==1)
+		{
+			postfix[j]=infix[i];
+			j++;
+		}
+		else
+		{
+			while(top!=-1 && priority<=get_priority(st[top]))
+			{
+				postfix[j]=st[top];
+				j++;
+				top--;
+			}
+			st[++top]=infix[i];
+		}
+	}
+	while(top!=-1)
+	{
+		postfix[j]=st[top];
+		j++;
+		top--;
+	}
+	postfix[j]='\0';
+	printf("Postfix Notation is: %s",postfix);
+}
+int main()
+{
+	char infix[101];
+	printf("Enter all expression in INFIX form: ");
+	scanf("%s",infix);
+	convert_infix_to_postfix(infix);
+	return 0;
+}
