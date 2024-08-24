af33bc0f766f21a6b51ff829b69fe133989f9db2
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-08-24 18:46:08 +0530

    #include <stdio.h>

---
+++ gold_coins.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int a,b,x,y;
+    scanf("%d%d%d%d",&a,&b,&x,&y);
+    if(x>y)
+    printf("%d",a);
+    else
+    printf("%d",b);
+}
