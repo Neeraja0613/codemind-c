fffaf860210c28264bedf3b6d5f08ff060566868
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-09-03 09:40:56 +0530

    #include <stdio.h>

---
+++ morning_run.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    z=2*(x+y);
+    if(z>=1000)
+    printf("YES");
+    else
+    printf("NO");
+}
