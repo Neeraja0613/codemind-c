272bcaffe067f98fa5f11139a8e7304167f0123f
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-09-05 10:02:34 +0530

    #include <stdio.h>

---
+++ icecream.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if((2*x)<=y)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
